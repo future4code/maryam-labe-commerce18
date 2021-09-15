32784ae1d4f5feb25a9b9e74232c69bdf2918fe9
matheus15255 <matheus_d.a.s@hotmail.com>
2021-09-15 12:27:19 -0300

    inicio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,136 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Filters } from "./components/Filters/Filters";
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const AppContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 3fr 1fr;
+  padding: 16px;
+  gap: 8px;
+`;
+
+const products = [
+  {
+    id: 1,
+    name: "Produto 1",
+    price: 100,
+    photo: "https://picsum.photos/200?a=1",
+  },
+  {
+    id: 2,
+    name: "Produto 2",
+    price: 200,
+    photo: "https://picsum.photos/200?a=2",
+  },
+  {
+    id: 3,
+    name: "Produto 3",
+    price: 300,
+    photo: "https://picsum.photos/200?a=3",
+  },
+  {
+    id: 4,
+    name: "Produto 4",
+    price: 400,
+    photo: "https://picsum.photos/200?a=4",
+  },
+];
+
+class App extends React.Component {
+  state = {
+    minFilter: 100,
+    maxFilter: 1000,
+    nameFilter: "Produto",
+    productsInCart: [
+      {
+        id: 4,
+        name: "Produto 4",
+        price: 400,
+        photo: "https://picsum.photos/200?a=4",
+        quantity: 1,
+      },
+      {
+        id: 3,
+        name: "Produto 3",
+        price: 300,
+        photo: "https://picsum.photos/200?a=3",
+        quantity: 3,
+      },
+    ],
+  };
+
+  onChangeMinFilter = (event) => {
+    this.setState({ minFilter: event.target.value });
+  };
+
+  onChangeMaxFilter = (event) => {
+    this.setState({ maxFilter: event.target.value });
+  };
+
+  onChangeNameFilter = (event) => {
+    this.setState({ nameFilter: event.target.value });
+  };
+
+  onAddProductToCart = (productId) => {
+    const productInCart = this.state.productsInCart.find(
+      (product) => productId === product.id
+    );
+
+    if (productInCart) {
+      const newProductsInCart = this.state.productsInCart.map((product) => {
+        if (productId === product.id) {
+          return {
+            ...product,
+            quantity: product.quantity + 1,
+          };
+        }
+
+        return product;
+      });
+
+      this.setState({ productsInCart: newProductsInCart });
+    } else {
+      const productToAdd = products.find((product) => productId === product.id);
+
+      const newProductsInCart = [
+        ...this.state.productsInCart,
+        { ...productToAdd, quantity: 1 },
+      ];
+
+      this.setState({ productsInCart: newProductsInCart });
+    }
+  };
+
+  onRemoveProductFromCart = (productId) => {
+    const newProductsInCart = this.state.productsInCart
+      .map((product) => {
+        if (product.id === productId) {
+          return {
+            ...product,
+            quantity: product.quantity - 1,
+          };
+        }
+        return product;
+      })
+      .filter((product) => product.quantity > 0);
+
+    this.setState({ productsInCart: newProductsInCart });
+  };
+
+  render() {
+    return (
+      <AppContainer>
+        <Filters
+          minFilter={this.state.minFilter}
+          maxFilter={this.state.maxFilter}
+          nameFilter={this.state.nameFilter}
+          onChangeMinFilter={this.onChangeMinFilter}
+          onChangeMaxFilter={this.onChangeMaxFilter}
+          onChangeNameFilter={this.onChangeNameFilter}
+        />
+      </AppContainer>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Filters/Filters.js
@@ -0,0 +1,49 @@
+import React from "react";
+import styled from "styled-components";
+
+const FiltersContainer = styled.div`
+  border: 1px solid black;
+  padding: 8px;
+  margin-right: 345px;
+`;
+
+const InputContainer = styled.label`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-bottom: 20px;
+`;
+
+export class Filters extends React.Component {
+  render() {
+    return (
+      <FiltersContainer>
+        <h3>Filtros</h3>
+        <InputContainer>
+          Valor mínimo:
+          <input
+            onChange={this.props.onChangeMinFilter}
+            value={this.props.minFilter}
+            type="number"
+          />
+        </InputContainer>
+        <InputContainer>
+          Valor máximo:
+          <input
+            onChange={this.props.onChangeMaxFilter}
+            value={this.props.maxFilter}
+            type="number"
+          />
+        </InputContainer>
+        <InputContainer>
+          Buscar por nome:
+          <input
+            onChange={this.props.onChangeNameFilter}
+            value={this.props.nameFilter}
+            type="text"
+          />
+        </InputContainer>
+      </FiltersContainer>
+    );
+  }
+}
